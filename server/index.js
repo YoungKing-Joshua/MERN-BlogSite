0482e97448456adcaf0b71ea54917b71f3da9c50
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-10-28 15:28:32 +0000

    MERN Blog Site

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config();
+main().then(console.log("Connected to Mongo")).catch(err => console.log(err));
 
-app.use("/", (req, res) =>
+async function main ()
 {
-    console.log("YoungKing");
+    await mongoose.connect(process.env.MONGO_URL);
+    useCreateIndex: true;
+}
 
-});
 
 app.listen("5000", () =>
 {
