0bfef3bca251ed58c4fec77b6a424721532a4771
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-10-29 18:00:34 +0000

    MERN Blog Site

--- server/index.js
+++ server/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
+
 
 dotenv.config();
+app.use(express.json());
 main().then(console.log("Connected to Mongo")).catch(err => console.log(err));
 
 async function main ()
 {
     await mongoose.connect(process.env.MONGO_URL);
-    useCreateIndex: true;
+    // useCreateIndex: true;
 }
 
+app.use("/api/auth", authRouter)
 
 app.listen("5000", () =>
 {
