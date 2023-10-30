11eb905a108b2a9091d53107cecad190105294c8
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-10-30 22:08:09 +0000

    MERN Blog Site

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const { response } = require("express");
-const User = require("../models/User.js");
+const Post = require("../models/Post");
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-//REGISTER
+//UPDTE
 router.put("/:id", async (req, res) =>
 {
     if (req.body.userId === req.params.id)
@@ -17,11 +18,12 @@ router.put("/:id", async (req, res) =>
         {
             const updateUser = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
-            });
-            res.status(200).json(updateUser)
+            },
+                { new: true });
+            res.status(200).json(updateUser);
         } catch (err)
         {
-
+            res.status(500).json(err);
         }
     } else
     {
@@ -31,3 +33,44 @@ router.put("/:id", async (req, res) =>
 
 
 module.exports = router;
+
+//DELETE
+router.delete("/:id", async (req, res) =>
+{
+    if (req.body.userId === req.params.id)
+    {
+        try
+        {
+            const user = await User.findById(req.params.id);
+            const { password, ...others } = user._doc;
+            try
+            {
+                await Post.deleteMany({ username: user.username });
+                await User.findByIdAndDelete(req.params.id);
+                res.status(200).json("User has been delete");
+            } catch (err)
+            {
+                res.status(500).json(err);
+            }
+        } catch (err) { response.status(404).json("User not found!"); }
+    } else
+    {
+        response.status(401).json("you can delete only your account!");
+    }
+});
+
+
+//GET USER
+router.get("/:id", async (req, res) =>
+{
+    try
+    {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+
+    } catch (err)
+    {
+        res.status(500).json(err);
+    }
+});
