6c2315cfe0486d14eb107fa2ee9844a1a287e641
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-10-31 17:46:47 +0000

    MERN Blog Site

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
 const postRouter = require("./routes/posts");
-
+const catRouter = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -17,9 +18,26 @@ async function main ()
     // useCreateIndex: true;
 }
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>
+    {
+        cb(null, "images");
+    }, filename: (req, file, cb) =>
+    {
+        cb(null, req.body.name);
+    }
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) =>
+{
+    res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/categories", catRouter);
 
 app.listen("5000", () =>
 {
