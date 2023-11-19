a2d7c7a17777302947b242f4a7c921e704b82c8c
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-11-19 10:10:05 +0000

    MERN Blog Site

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import Header from "../../component/header/header";
 import Posts from "../../component/posts/Posts";
 import Sidebar from "../../component/sidebar/Sidebar";
+import axios from "axios";
 
 export default function Home() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			axios.get("/");
+		};
+	}, []);
 	return (
 		<>
 			<Header />
 			<div className='home'>
-            <Posts />
-            <Sidebar />
-            </div>
+				<Posts />
+				<Sidebar />
+			</div>
 		</>
 	);
 }
