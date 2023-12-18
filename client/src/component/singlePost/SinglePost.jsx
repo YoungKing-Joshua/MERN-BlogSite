629099051dcbbe902ec03b1ee5d487f3073950fe
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-12-18 13:16:06 +0000

    Blog Site

--- client/src/component/singlePost/SinglePost.jsx
+++ client/src/component/singlePost/SinglePost.jsx
@@ -1,41 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./singlePost.css";
+import { useLocation } from "react-router-dom";
 
 //A
 
 export default function SinglePost() {
+	const location = useLocation();
+	const path = location.pathname.split("/")[2];
+	const [post, setPost] = useState({});
+
+	useEffect(() => {
+		const getPost = async () => {
+			const res = await axios.get("http://localhost:5000/api/posts/" + path);
+			setPost(res.data);
+		};
+		getPost();
+	}, [path]);
+
 	return (
 		<div className='singlePost'>
-			<img
-				className='singlePosting'
-				src='https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-				alt=''
-			/>
-			<h1 className='singlePostingTitle'>
-				Lorem ipsum dolor sit amet.
-				<div className='singlePostEdit'>
-					<i className='singlePostIcon far fa-edit'></i>
-					<i className='singlePostIcon far fa-trash-alt'></i>
+			<div className='singlePostWrapper'>
+				{post.photo && <img className='singlePosting' src={post.photo} alt='' />}
+				<h1 className='singlePostingTitle'>
+					{post.title}
+					<div className='singlePostEdit'>
+						<i className='singlePostIcon far fa-edit'></i>
+						<i className='singlePostIcon far fa-trash-alt'></i>
+					</div>
+				</h1>
+
+				<div className='singlePostInfo'>
+					<span className='singlePostAuthor'>
+						Author: <b>{post.username}</b>
+					</span>
+					<span className='singlePostDate'>{new Date(post.createdAt).toDateString()}</span>
 				</div>
-			</h1>
-			<div className='singlePostInfo'>
-				<span className='singlePostAuthor'>
-					Author: <b>Joshua Dei-Alorse</b>
-				</span>
-				<span className='singlePostDate'>1 hour ago</span>
+				<p className='singlePostDesc'>{post.desc}</p>
 			</div>
-			<p className='singlePostDesc'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Est maiores veniam molestias vel
-				numquam quam totam, saepe id alias mollitia, quisquam nihil cumque! Commodi deleniti magni
-				alias id asperiores blanditiis.lorem Lorem, ipsum dolor sit amet consectetur adipisicing
-				elit. Temporibus libero a incidunt dolorem reprehenderit voluptas sint aut autem dicta,
-				atque ratione eum iusto possimus quisquam quidem ducimus vel vero suscipit. Lorem ipsum
-				dolor sit, amet consectetur adipisicing elit. Sequi voluptatum atque illo perspiciatis
-				soluta. Maiores delectus quo voluptates dicta numquam accusamus iure vero, provident
-				exercitationem temporibus maxime, quisquam, perferendis eos! Lorem, ipsum dolor sit amet
-				consectetur adipisicing elit. Ipsam incidunt itaque repellendus saepe harum sed excepturi
-				est ratione ab placeat distinctio aliquam quos modi hic eum voluptatem, aut tempore vero!
-			</p>
 		</div>
 	);
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,10 +4,15 @@ import Header from "../../component/header/header";
 import Posts from "../../component/posts/Posts";
 import Sidebar from "../../component/sidebar/Sidebar";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
+
 
 export default function Home() {
 	const [posts, setPosts] = useState([]);
+    const location = useLocation()
 
+
+	
 	useEffect(() => {
 		const fetchPosts = async () => {
 			const res = await axios.get("http://localhost:5000/api/posts");
